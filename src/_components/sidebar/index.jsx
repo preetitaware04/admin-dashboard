104f480dee1358b08cb07f75d180ebca9a211c2b
preetitaware04 <preetitaware2001@gmail.com>
2025-06-02 18:20:45 +0530

    sidebar wip

--- src/_components/sidebar/index.jsx
+++ src/_components/sidebar/index.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { sidebarMenu } from "../sidebar.constants";
 import { Button } from "@mui/material";
+import { FaAngleDown } from "react-icons/fa6";
 
 const Sidebar = () => {
   return (
@@ -15,14 +16,21 @@ const Sidebar = () => {
           <ul className="w-full">
             {sidebarMenu?.map((menu, index) => {
               return (
-                <div className="w-full" key={menu?.id}>
+                <div className="w-full relative group" key={menu?.id}>
                   <Button
                     variant="text"
-                    className="w-full !capitalize text-left !justify-start !text-gray-700  gap-2 !font-bold !text-[13px] !py-3 dark:!text-gray-200"
+                    className="w-full !capitalize text-left !justify-start group-hover:!bg-gray-200 !text-medium  gap-2 !font-[600] !text-[13px] !py-[8px] dark:!text-gray-200"
                   >
                     {menu?.icon}
                     {`${menu?.title}`}
                   </Button>
+
+                  {menu?.items?.length > 0 && (
+                    <span className="absolute w-[40px] h-[40px] z-[50] top-[5px] right-0 flex items-center justify-center cursor-pointer group-hover:!bg-gray-300">
+                      <FaAngleDown />
+                    </span>
+                  )}
+
                   {menu?.items?.length > 0 && (
                     <div className="submenu w-full flex flex-col py-1">
                       {menu?.items?.map((item, index) => {
@@ -34,8 +42,9 @@ const Sidebar = () => {
                           >
                             <Button
                               variant="text"
-                              className="block !w-full !text-[13px] !capitalize hover:bg-gray-200 !text-left !justify-start !text-medium"
+                              className="!w-full !text-[13px] !capitalize !hover:bg-gray-200 !text-left !justify-start !text-medium gap-2 !pl-3"
                             >
+                              <span className="w-[5px] h-[5px] rounded-full bg-gray-500"></span>
                               {item?.title}
                             </Button>
                           </Link>
