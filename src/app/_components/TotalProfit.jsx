bbe2ea314238cff7d9e55bbe6db574aa74f4ac33
preetitaware04 <preetitaware2001@gmail.com>
2025-07-14 18:19:58 +0530

    new changes

---
+++ src/app/_components/TotalProfit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalProfit = () => {
+  return (
+    <div className="p-5 shadow-md ">
+      <h2 className="text-lg font-bold">TotalProfit</h2>
+    </div>
+  );
+};
+
+export default TotalProfit;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Banner from "@/app/_components/Banner";
 import DashboardCards from "./_components/DashboardCards";
+import TotalProfit from "./_components/TotalProfit";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <DashboardCards />
+      <DashboardCards/>
+    <div className="grid grid-cols-2 gap-4 my-4">
+      <TotalProfit/>
+    </div>
     </>
   );
 }
