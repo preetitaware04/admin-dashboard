3751ae26a792ef34fed5f634072528a3de0ad7dd
preetitaware04 <preetitaware2001@gmail.com>
2025-07-17 18:21:19 +0530

    channel revenue section wip

--- src/app/_components/TotalProfit.jsx
+++ src/app/_components/TotalProfit.jsx
@@ -1,9 +1,89 @@
+"use client";
+import { Button } from "@mui/material";
 import React from "react";
+import {
+  AreaChart,
+  Area,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
+const profitData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 const TotalProfit = () => {
   return (
-    <div className="p-5 shadow-md ">
-      <h2 className="text-lg font-bold">TotalProfit</h2>
+    <div className="p-5 shadow-md rounded-md">
+      <div className="flex justify-between items-start">
+        <div>
+          <h2 className="text-lg">TotalProfit</h2>
+          <span className="text-[25px] font-bold">$8,950.00</span>
+        </div>
+        <Button variant="outlined" className="!capitalize" size="small">
+          Details
+        </Button>
+      </div>
+      <div className="w-full h-[250px]">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart width={500} height={400} data={profitData}>
+            <defs>
+              <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              strokeWidth={3}
+              fill="url(#colorValue)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import Banner from "@/app/_components/Banner";
 import DashboardCards from "./_components/DashboardCards";
 import TotalProfit from "./_components/TotalProfit";
+import ChannelRevenue from "./_components/ChannelRevenue";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <DashboardCards/>
-    <div className="grid grid-cols-2 gap-4 my-4">
-      <TotalProfit/>
-    </div>
+      <DashboardCards />
+      <div className="grid grid-cols-2 gap-4 my-4 px-5">
+        <TotalProfit />
+        <ChannelRevenue />
+      </div>
     </>
   );
 }
